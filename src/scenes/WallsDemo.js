b706e12a56410e6f6604619104c0b68eea2b9d30
Gus700 <gcruzmar@ucsc.edu>
2022-05-15 22:44:03 -0700

    added basic buttons to the different scene demos in our menu

--- src/scenes/WallsDemo.js
+++ src/scenes/WallsDemo.js
@@ -2,8 +2,24 @@ class WallsDemo extends Phaser.Scene {
     constructor() {
         super("wallsDemoScene");
     }
+    
+    preload(){
+        this.load.image("wall", "./assets/Metal Wall 1 temp.png");
+    }
 
     create() {
+        this.wallTileWidth = this.textures.get("wall").getSourceImage().width;
+        this.wallTileHeight = this.textures.get("wall").getSourceImage().height;
+        this.wall = this.physics.add.existing(this.add.tileSprite(
+            globalGameConfig.width/2,  
+            globalGameConfig.height/2, 
+            this.wallTileWidth * ((globalGameConfig.width) / this.wallTileWidth), 
+            this.wallTileHeight * ((globalGameConfig.height) / this.wallTileHeight),
+            "wall"));
+        this.wall
+    }
+
+    makeSquareRoom(x, y, roomWidth, roomHeight, wallThickness) {
         
     }
 }
