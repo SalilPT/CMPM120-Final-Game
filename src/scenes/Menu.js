2bf385e2bd9396b232895789ad8123614d157c59
Gus700 <gcruzmar@ucsc.edu>
2022-05-15 23:42:09 -0700

    actually added buttons for the scenes, last push was wrong commit message

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,9 +4,32 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
+        let demoSceneTransitionTextConfig = {color: "black", fontSize: "36px", stroke: "white", strokeThickness: 0};
         // Make buttons or keybinds to go to demo scenes
-        let demoSceneTransitionTextConfig = {color: "black", fontSize: "36px", stroke: "white", strokeThickness: 1};
-        this.add.text(32, 64, "Press 2 (non-numpad) to go to Movement and Aiming Demo", demoSceneTransitionTextConfig);
+        this.MovementDemButton = this.add.rectangle(globalGameConfig.width/4, globalGameConfig.height/2, globalGameConfig.width/6, globalGameConfig.height/8, 0x6666ff);
+        this.PuzzleDemoButton = this.add.rectangle(globalGameConfig.width/2, globalGameConfig.height/2, globalGameConfig.width/6, globalGameConfig.height/8, 0x6666ff);
+        this.WallsDemoButton = this.add.rectangle(globalGameConfig.width/1.33, globalGameConfig.height/2, globalGameConfig.width/6, globalGameConfig.height/8, 0x6666ff);
+        
+        this.add.text(globalGameConfig.width/4, globalGameConfig.height/2, "Movement/\nAiming Demo", demoSceneTransitionTextConfig).setOrigin(0.5);
+        this.add.text(globalGameConfig.width/2, globalGameConfig.height/2, "Puzzle Demo", demoSceneTransitionTextConfig).setOrigin(0.5);
+        this.add.text(globalGameConfig.width/1.33, globalGameConfig.height/2, "Walls Demo", demoSceneTransitionTextConfig).setOrigin(0.5);
+
+        this.MovementDemButton.setInteractive();
+        this.PuzzleDemoButton.setInteractive();
+        this.WallsDemoButton.setInteractive();
+
+        this.MovementDemButton.on("pointerdown", () =>{
+            this.scene.start("movementAndAimingDemoScene");
+        })
+
+        this.PuzzleDemoButton.on("pointerdown", () =>{
+            this.scene.start("puzzleDemoScene");
+        })
+
+        this.WallsDemoButton.on("pointerdown", () =>{
+            this.scene.start("wallsDemoScene");
+        })
+
         this.input.keyboard.on("keydown", (event) => {
             let keyCodeStr = event.keyCode.toString();
             console.log(Phaser.Input.Keyboard.KeyCodes.TWO.toString())
