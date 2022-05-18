97d1dfeab3d2583c6dbb1f6bc782abd6cb5b58d8
Gus700 <gcruzmar@ucsc.edu>
2022-05-18 02:34:27 -0700

    Able to display a basic level using a tilemap

--- src/scenes/WallsDemo.js
+++ src/scenes/WallsDemo.js
@@ -5,27 +5,34 @@ class WallsDemo extends Phaser.Scene {
     
     preload(){
         this.load.image("wall", "./assets/Metal Wall 1 temp.png");
+        //load tileset that is used by the tile map
+        this.load.image("bTileSet", "./assets/bulletHellTileSet.png"); // tile sheet
+        //load jsonfile used by the tilemap
+        this.load.tilemapTiledJSON("tileMap", "./assets/bulletHellMap.json");    // Tiled JSON file
     }
 
     create() {
+        
+        // add tilemap
+        const map = this.add.tilemap("tileMap");
+        //set a tileset for the map
+        const tileset = map.addTilesetImage("bulletHellTileSet", "bTileSet");
+        const wallLayer = map.createLayer("Walls", tileset, 0, 0);
+        const floorLayer = map.createLayer("Floor", tileset, 0, 0);
 
+        wallLayer.setCollisionByProperty({
+            collides: true
+        });
 
-        this.wallTileWidth = this.textures.get("wall").getSourceImage().width;
-        this.wallTileHeight = this.textures.get("wall").getSourceImage().height;
-        this.wall = this.physics.add.existing(this.add.tileSprite(
-            globalGameConfig.width/2,  
-            globalGameConfig.height/2, 
-            this.wallTileWidth * ((globalGameConfig.width) / this.wallTileWidth), 
-            this.wallTileHeight * ((globalGameConfig.height) / this.wallTileHeight),
-            "wall"));
-        this.wall
+        
+
 
         let debugTextConfig = {color: "white", fontSize: "50px", stroke: "black", strokeThickness: 1};
         this.add.text(globalGame.config.width - 32, globalGame.config.height - 64, "Press 0 (non-numpad) to go back to Menu", debugTextConfig).setOrigin(1, 0);
         this.input.keyboard.on("keydown-ZERO", () => {this.scene.start("menuScene");});
     }
 
-    makeSquareRoom(x, y, roomWidth, roomHeight, wallThickness) {
+    /*makeSquareRoom(x, y, roomWidth, roomHeight, wallThickness) {
         
-    }
+    }*/
 }
