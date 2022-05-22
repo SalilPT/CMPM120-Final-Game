fbf5a68965d521d0aa614b022a3606b09fb0bed4
Gus700 <gcruzmar@ucsc.edu>
2022-05-21 19:39:51 -0700

    added shooting to the tutorial level, bullets collide with walls

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -4,11 +4,15 @@ class Tutorial extends Phaser.Scene {
     }
     
     preload(){
+        //visual assets
         this.load.image("wall", "./assets/Metal Wall 1 temp.png");
         this.load.spritesheet("bSpritesheet", "./assets/bulletHellTileSet.png", {frameWidth: 64, frameHeight:64}); // tile sheet
         // load tileset that is used by the tile map and load jsonfile used by the tilemap
         this.load.image("bTileSet", "./assets/bulletHellTileSet.png"); // tile sheet
         this.load.tilemapTiledJSON("tileMap", "./assets/bulletHellMap.json");    // Tiled JSON file
+        
+        //audio assets
+        this.load.audio("shootingSFX", "./assets/audio/shooting_sfx.wav");
     }
 
     create() {
@@ -27,13 +31,29 @@ class Tutorial extends Phaser.Scene {
         this.jebPlayer.body.setCollideWorldBounds(true);
         this.jebPlayer.setCircle(32); // make collsion into circle shape
         //create the collider and instance of the movement manager
-        this.physics.add.collider(this.jebPlayer, wallLayer);
+        this.collidesWithWalls = this.physics.add.group();
+        this.collidesWithWalls.add(this.jebPlayer);
+        this.physics.add.collider(this.collidesWithWalls, wallLayer);
         this.movementMan = new PlayerMovementManager(this);
         this.movementMan.setMovSpd(300);
         // changing scenes debubgger
         let debugTextConfig = {color: "white", fontSize: "50px", stroke: "black", strokeThickness: 1};
         this.add.text(globalGame.config.width - 32, globalGame.config.height - 64, "Press 0 (non-numpad) to go back to Menu", debugTextConfig).setOrigin(1, 0);
         this.input.keyboard.on("keydown-ZERO", () => {this.scene.start("menuScene");});
+        //code based off the shooting demo
+        this.playerBulletGroup = this.add.group();
+        this.input.on("pointerdown", () => {
+            let newPlayerBullet = this.physics.add.sprite(this.jebPlayer.x, this.jebPlayer.y, "bSpritesheet", 4).setOrigin(0.5);
+            this.playerBulletGroup.add(newPlayerBullet);
+            this.collidesWithWalls.add(newPlayerBullet);
+            let fireAngle = Phaser.Math.Angle.Between(this.jebPlayer.body.center.x, this.jebPlayer.body.center.y, this.input.activePointer.worldX, this.input.activePointer.worldY);
+            fireAngle = Phaser.Math.RadToDeg(fireAngle);
+            let fireVector = this.physics.velocityFromAngle(fireAngle, 250);
+            newPlayerBullet.body.setVelocity(fireVector.x, fireVector.y);
+            newPlayerBullet.setScale(0.5);
+            this.sound.play("shootingSFX");
+        });
+
     }
 
     update(){
