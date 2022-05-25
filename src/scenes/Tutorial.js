9b410bd8683d3ccca64807f49359a809633a3284
Gus700 <gcruzmar@ucsc.edu>
2022-05-24 20:14:21 -0700

    changed tutorial so all visual assets come from an atlas

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -5,14 +5,8 @@ class Tutorial extends Phaser.Scene {
     
     preload(){
         //visual assets
-        this.load.image("wall", "./assets/Metal Wall 1 temp.png");
-        this.load.spritesheet("bSpritesheet", "./assets/bulletHellTileSet.png", {frameWidth: 64, frameHeight:64}); // tile sheet
-        this.load.image("puzPieceSprite", "./assets/Key Temp.png");
-        this.load.image("puzHoleSprite", "./assets/Enemy Temp.png");
-        // load tileset that is used by the tile map and load jsonfile used by the tilemap
-        this.load.image("bTileSet", "./assets/bulletHellTileSet.png"); // tile sheet
-        this.load.tilemapTiledJSON("tileMap", "./assets/bulletHellMap.json");    // Tiled JSON file
-        
+        this.load.atlas("gameAtlas", "./assets/bulletHellTileSet.png", "./assets/bulletHellTileSet.json");
+        this.load.tilemapTiledJSON("tileMap", "./assets/bulletHellMap.json"); // tile map JSON file (from tiled software)
         //audio assets
         this.load.audio("shootingSFX", "./assets/audio/shooting_sfx.wav");
     }
@@ -21,7 +15,7 @@ class Tutorial extends Phaser.Scene {
         // mainly followed Nathan Altice's mappy example for collisions using tile maps
         const map = this.add.tilemap("tileMap");
         // set a tileset for the map and its corresponding layers
-        const tileset = map.addTilesetImage("bulletHellTileSet", "bTileSet");
+        const tileset = map.addTilesetImage("bulletHellTileSet", "gameAtlas");
         const floorLayer = map.createLayer("Floor", tileset, 0, 0);
         const wallLayer = map.createLayer("Walls", tileset, 0, 0);
         floorLayer.setDepth(-2); // change as needed, note:puzzle piece sprite is 1 less than player depth, and keyhole is 2 less
@@ -32,11 +26,10 @@ class Tutorial extends Phaser.Scene {
         //collision group
         this.anythingAndWalls = this.physics.add.group(); // group to keep things inside the walls
         // create a player
-        this.jebPlayer = this.physics.add.sprite(globalGameConfig.width/4, globalGameConfig.height/2, "bSpritesheet", 2);
+        this.jebPlayer = this.physics.add.sprite(globalGameConfig.width/4, globalGameConfig.height/2, "gameAtlas", "jeb legs temp.png");
         this.jebPlayer.body.setCollideWorldBounds(true);
-        this.jebPlayer.setCircle(this.textures.getFrame("bSpritesheet", 2).width/2); // make collsion into circle shape
+        this.jebPlayer.setCircle(this.textures.getFrame("gameAtlas", "jeb legs temp.png").width/2); // make collsion into circle shape
         this.anythingAndWalls.add(this.jebPlayer);
-
         //create the collider and instance of the movement manager
         this.physics.add.collider(this.anythingAndWalls, wallLayer, (object1) => {
             if(object1 != this.jebPlayer){ // check it is not player sprite
@@ -45,15 +38,13 @@ class Tutorial extends Phaser.Scene {
         });
         this.movementMan = new PlayerMovementManager(this);
         this.movementMan.setMovSpd(400);
-
         // changing scenes debubgger
         let debugTextConfig = {color: "white", fontSize: "50px", stroke: "black", strokeThickness: 1};
         this.add.text(globalGame.config.width - 32, globalGame.config.height - 64, "Press 0 (non-numpad) to go back to Menu", debugTextConfig).setOrigin(1, 0);
         this.input.keyboard.on("keydown-ZERO", () => {this.scene.start("menuScene");});
-
         //code based off the shooting demo
         this.input.on("pointerdown", () => {
-            let newPlayerBullet = this.physics.add.sprite(this.jebPlayer.x, this.jebPlayer.y, "bSpritesheet", 4).setOrigin(0.5);
+            let newPlayerBullet = this.physics.add.sprite(this.jebPlayer.x, this.jebPlayer.y, "gameAtlas", "Key Temp.png").setOrigin(0.5);
             this.anythingAndWalls.add(newPlayerBullet);
             let fireAngle = Phaser.Math.Angle.Between(this.jebPlayer.body.center.x, this.jebPlayer.body.center.y, this.input.activePointer.worldX, this.input.activePointer.worldY);
             fireAngle = Phaser.Math.RadToDeg(fireAngle);
@@ -62,7 +53,6 @@ class Tutorial extends Phaser.Scene {
             newPlayerBullet.setScale(0.5);
             this.sound.play("shootingSFX");
         });
-
         //code based on the puzzle demo
         this.puzzleMan = new PuzzleManager(this, {playerChar: this.jebPlayer});
         //let thing = this.puzzleMan.interactKeyObj;
@@ -75,12 +65,12 @@ class Tutorial extends Phaser.Scene {
                 scene: this,
                 x: 128*i,
                 y: 800 + 64 * Math.pow(-1, i),
-                texture: "bSpritesheet",
-                frame: 4
+                texture: "gameAtlas",
+                frame: "Key Temp.png"
             }).setOrigin(0);
             newPiece.numInSequence = i;
             this.puzzleMan.addPuzzlePieceToSeq(newPiece, seqIndex);
-            let newPuzHole = this.physics.add.sprite(128*i, 128 + 64 * Math.pow(-1, i), "bSpritesheet", 0).setOrigin(0);
+            let newPuzHole = this.physics.add.sprite(128*i, 128 + 64 * Math.pow(-1, i), "gameAtlas", "Enemy Temp.png").setOrigin(0);
             newPuzHole.numInSequence = i;
             this.puzzleMan.addHoleToSeq(newPuzHole, seqIndex);
         }
