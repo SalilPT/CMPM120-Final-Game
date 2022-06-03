6ca85d3527b4b1a8bd215597a00b4ecdddf4e6da
Gus700 <gcruzmar@ucsc.edu>
2022-06-02 21:54:52 -0700

    updated menu screen text and colors

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,29 +5,37 @@ class Menu extends Phaser.Scene {
 
     create() {
         this.scene.bringToTop();
-        let demoSceneTransitionTextConfig = {color: "black", fontSize: "40px", stroke: "white", strokeThickness: 0};
+        // text configuration
+        let menuTextConfig = {
+            fontFamily: "bulletFont",
+            fontSize: "60px",
+            color: "#F7F6F3",
+            stroke: "#160F29",
+            strokeThickness: 4
+        }
         // Make buttons or keybinds to go to demo scenes
 
-        this.add.rectangle(globalGameConfig.width/2, globalGameConfig.height/2, globalGameConfig.width/6, globalGameConfig.height/8, 0x6666ff)
+        this.add.rectangle(globalGameConfig.width/2, globalGameConfig.height/2, globalGameConfig.width/6, globalGameConfig.height/8, 0x00BFB2)
             .setInteractive({useHandCursor: true})
             .on("pointerdown", () => this.scene.start("tutorialScene"));
         
-        this.add.rectangle(globalGameConfig.width/3, globalGameConfig.height/1.3, globalGameConfig.width/11, globalGameConfig.height/16, 0x6666ff)
+        this.add.rectangle(globalGameConfig.width/3, globalGameConfig.height/1.3, globalGameConfig.width/7, globalGameConfig.height/9, 0x9ADFEA)
             .setInteractive({useHandCursor: true})
             .on("pointerdown", () => this.scene.start("movementAndAimingDemoScene"));
         
-        this.add.rectangle(globalGameConfig.width/2, globalGameConfig.height/1.3, globalGameConfig.width/11, globalGameConfig.height/16, 0x6666ff)
+        this.add.rectangle(globalGameConfig.width/2, globalGameConfig.height/1.3, globalGameConfig.width/7, globalGameConfig.height/9, 0x9ADFEA)
             .setInteractive({useHandCursor: true})
             .on("pointerdown", () => this.scene.start("puzzleDemoScene"));
 
-        this.add.rectangle(globalGameConfig.width*(2/3), globalGameConfig.height/1.3, globalGameConfig.width/11, globalGameConfig.height/16, 0x6666ff)
+        this.add.rectangle(globalGameConfig.width*(2/3), globalGameConfig.height/1.3, globalGameConfig.width/7, globalGameConfig.height/9, 0x9ADFEA)
         .setInteractive({useHandCursor: true})
         .on("pointerdown", () => this.scene.start("bulletsDemoScene"));
                     
-        this.add.text(globalGameConfig.width/2, globalGameConfig.height/2, "tutorial", demoSceneTransitionTextConfig).setOrigin(0.5);
-        demoSceneTransitionTextConfig.fontSize = '20px';
-        this.add.text(globalGameConfig.width/3, globalGameConfig.height/1.3, "Movement/\nAiming Demo", demoSceneTransitionTextConfig).setOrigin(0.5);
-        this.add.text(globalGameConfig.width/2, globalGameConfig.height/1.3, "Puzzle Demo", demoSceneTransitionTextConfig).setOrigin(0.5);
-        this.add.text(globalGameConfig.width*(2/3), globalGameConfig.height/1.3, "Bullets Demo", demoSceneTransitionTextConfig).setOrigin(0.5);
+        this.add.text(globalGameConfig.width/2, globalGameConfig.height/2, "Tutorial", menuTextConfig).setOrigin(0.5);
+        menuTextConfig.fontSize = '30px';
+        menuTextConfig.strokeThickness = 4;
+        this.add.text(globalGameConfig.width/3, globalGameConfig.height/1.3, "Movement/\nAiming Demo", menuTextConfig).setOrigin(0.5);
+        this.add.text(globalGameConfig.width/2, globalGameConfig.height/1.3, "Puzzle Demo", menuTextConfig).setOrigin(0.5);
+        this.add.text(globalGameConfig.width*(2/3), globalGameConfig.height/1.3, "Bullets Demo", menuTextConfig).setOrigin(0.5);
     }
 }
