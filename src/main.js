5c1e4b1e48401e5ba8648848e622fe8f1a96934a
Gus700 <gcruzmar@ucsc.edu>
2022-06-08 20:15:49 -0700

    Changed the button sprites, and background color of the menu

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ let globalGameConfig = {
     type: Phaser.WEBGL,
     width: 1600,
     height: 900,
-    backgroundColor: 0x000000,
+    backgroundColor: 0x646E78,
     physics: {
         default: "arcade",
         arcade: {
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,20 +4,21 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('tealButton', './assets/tealButton.png')
     }
 
     create() {
         this.userInterfaceMgr = new UserInterfaceManager(this, {});
         const halfGameWidth = globalGameConfig.width/2;
         const halfGameHeight = globalGameConfig.height/2
-        this.userInterfaceMgr.createMenuButton(halfGameWidth, halfGameHeight, 288, 108, "Play", "playScene", undefined, {
+        this.userInterfaceMgr.createNewMenuButton(halfGameWidth, halfGameHeight, 1, "Play", "playScene", {
             levelsLeft: 1,
             completedLevels: []
         });
-        this.userInterfaceMgr.createMenuButton(halfGameWidth, halfGameHeight + 1*128, 256, 96, "Tutorial", "tutorialScene");
-        this.userInterfaceMgr.createMenuButton(halfGameWidth - 160, halfGameHeight + 2*128, 256, 96, "Settings", "settingsScene");
-        this.userInterfaceMgr.createMenuButton(halfGameWidth + 160, halfGameHeight + 2*128, 256, 96, "Credits", "creditsScene");
+
+        this.userInterfaceMgr.createNewMenuButton(halfGameWidth, halfGameHeight + 1*160, 1, "Tutorial", "tutorialScene");
+        this.userInterfaceMgr.createNewMenuButton(halfGameWidth - 160, halfGameHeight + 2*160, 1, "Settings", "settingsScene");
+        this.userInterfaceMgr.createNewMenuButton(halfGameWidth + 160, halfGameHeight + 2*160, 1, "Credits", "creditsScene");
 
         // Title Text
         this.add.text(halfGameWidth, halfGameHeight - 320, "Jeb's Puzzling Mission", 
