334bb1c4d42424074d74cc57012b0dec2d1c5353
Gus700 <gcruzmar@ucsc.edu>
2022-06-08 20:59:17 -0700

    fixed some bugs with menu beat and made tutorial instruction more clear

--- src/scenes/Settings.js
+++ src/scenes/Settings.js
@@ -11,7 +11,8 @@ class Settings extends Phaser.Scene {
         this.userInterfaceMgr = new UserInterfaceManager(this, {});
         const halfGameWidth = globalGameConfig.width/2;
         const halfGameHeight = globalGameConfig.height/2
-        this.userInterfaceMgr.createMenuButton(128, globalGameConfig.height - 128, 256, 96, "Back", "menuScene");
+        this.userInterfaceMgr.createMenuButton(128, globalGameConfig.height - 128, 256, 96, "Back", "menuScene")
+        .once("pointerdown", ()=>this.sound.removeByKey("menuBeat"));
     }
 
     update() {
