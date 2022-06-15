e4ec3a50ce09e092688bd2c87fc7b6d09769f497
SalilPT <32131426+SalilPT@users.noreply.github.com>
2022-06-15 07:59:08 -0700

    Fixed level progression not working correctly after a full playthrough

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,7 +11,7 @@ class Menu extends Phaser.Scene {
         this.userInterfaceMgr = new UserInterfaceManager(this, {});
         const halfGameWidth = globalGameConfig.width/2;
         const halfGameHeight = globalGameConfig.height/2
-        this.userInterfaceMgr.createMenuButton(halfGameWidth, halfGameHeight, 288, 108, "Play", "playScene", undefined);
+        this.userInterfaceMgr.createMenuButton(halfGameWidth, halfGameHeight, 288, 108, "Play", "playScene", undefined, {}); // An empty object is passed as data here to the Play scene's init() method to make level progression work properly.
         this.userInterfaceMgr.createMenuButton(halfGameWidth, halfGameHeight + 1*128, 256, 96, "Tutorial", "tutorialScene");
         this.userInterfaceMgr.createMenuButton(halfGameWidth - 160, halfGameHeight + 2*128, 256, 96, "Settings", "settingsScene");
         this.userInterfaceMgr.createMenuButton(halfGameWidth + 160, halfGameHeight + 2*128, 256, 96, "Credits", "creditsScene");
